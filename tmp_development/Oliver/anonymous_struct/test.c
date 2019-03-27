bd7155cacf8e015c8257f7a017ff3a7f1655e11c
O.R <oliver894@gmail.com>
2019-03-27 20:28:49 +0100

    added anonymous struct example and modified random_notes

---
+++ tmp_development/Oliver/anonymous_struct/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main(void){
+
+		union Scope{
+				struct {
+					int doors;
+					int housenumber;
+					char *name;
+					bool on_fire;
+				};
+		} Scope;
+	Scope.doors = 3;
+	Scope.name = "Karl";
+	printf("House has %d doors and is called %s\n",Scope.doors,Scope.name);
+
+return EXIT_SUCCESS;
+}
