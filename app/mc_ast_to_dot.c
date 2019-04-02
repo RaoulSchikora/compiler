72338c33b831e61975cb7120364b917dc7b58545
Raoul Jaser Schikora <raoul.schikora@student.uibk.ac.at>
2019-04-02 16:34:57 +0200

    mc_ast_to_dot can read files, command '-' reads input from stdin

--- app/mc_ast_to_dot.c
+++ app/mc_ast_to_dot.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mcc/ast.h"
 #include "mcc/ast_print.h"
 #include "mcc/parser.h"
 
-int main(void)
+void print_usage(const char *prg)
 {
+	printf("usage: %s <FILE>\n\n", prg);
+	printf("  <FILE>        Input filepath or - for stdin\n");
+}
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2) {
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	// determine input source
+	FILE *in;
+	if (strcmp("-", argv[1]) == 0) {
+		in = stdin;
+	} else {
+		in = fopen(argv[1], "r");
+		if (!in) {
+			perror("fopen");
+			return EXIT_FAILURE;
+		}
+	}
 	struct mcc_ast_expression *expr = NULL;
 
 	// parsing phase
 	{
-		struct mcc_parser_result result = mcc_parse_file(stdin);
+		struct mcc_parser_result result = mcc_parse_file(in);
+		fclose(in);
 		if (result.status != MCC_PARSER_STATUS_OK) {
 			return EXIT_FAILURE;
 		}
