33d5c127c7b784bae3d43e5ccfef886ecffcba32
Raoul Jaser Schikora <raoul.schikora@student.uibk.ac.at>
2019-05-27 13:42:49 +0200

    symbol_table first draft: rows

---
+++ include/mcc/symbol_table.h
@@ -0,0 +1,8 @@
+//
+// Created by raoul on 24.05.19.
+//
+
+#ifndef COMPILER_SYMBOL_TABLE_H
+#define COMPILER_SYMBOL_TABLE_H
+
+#endif //COMPILER_SYMBOL_TABLE_H
---
+++ test/unit/symbol_table_test.c
@@ -0,0 +1,4 @@
+//
+// Created by raoul on 25.05.19.
+//
+
