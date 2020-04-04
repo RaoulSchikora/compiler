7fe9ea9b2dac8d35cde492ce750ebd36f453209b
O.R <oliver894@gmail.com>
2020-04-04 14:22:02 +0200

    Add cleanup. Relates #90

--- app/mcc.c
+++ app/mcc.c
@@ -145,6 +145,7 @@ int main(int argc, char *argv[])
 	mc_cl_parser_delete_command_line_parser(command_line);
 	mcc_ast_delete_result(&result);
 	mcc_symbol_table_delete_table(table);
+	mcc_semantic_check_delete_single_check(semantic_check);
 
 	// TODO:
 	// - run semantic checks
