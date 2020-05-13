00422957b615dd17e73fe33d3e8f09fc1cede666
O.R <oliver894@gmail.com>
2020-05-13 12:25:26 +0200

    Add unit test stub for assembly

---
+++ test/unit/asm_test.c
@@ -0,0 +1,49 @@
+#include <CuTest.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mcc/ast.h"
+#include "mcc/ir.h"
+#include "mcc/semantic_checks.h"
+#include "mcc/symbol_table.h"
+
+void test1(CuTest *tc)
+{
+	// Define test input and create IR
+	const char input[] = "int main(){return 42;}";
+	struct mcc_parser_result parser_result;
+	parser_result = mcc_parse_string(input, MCC_PARSER_ENTRY_POINT_PROGRAM);
+	CuAssertIntEquals(tc, parser_result.status, MCC_PARSER_STATUS_OK);
+	struct mcc_symbol_table *table = mcc_symbol_table_create((&parser_result)->program);
+	struct mcc_semantic_check *checks = mcc_semantic_check_run_all((&parser_result)->program, table);
+	CuAssertIntEquals(tc, checks->status, MCC_SEMANTIC_CHECK_OK);
+	struct mcc_ir_row *ir = mcc_ir_generate((&parser_result)->program, table);
+	CuAssertPtrNotNull(tc, ir);
+}
+
+void test2(CuTest *tc)
+{
+	// Define test input and create IR
+	const char input[] = "int main(){return 42;}";
+	struct mcc_parser_result parser_result;
+	parser_result = mcc_parse_string(input, MCC_PARSER_ENTRY_POINT_PROGRAM);
+	CuAssertIntEquals(tc, parser_result.status, MCC_PARSER_STATUS_OK);
+	struct mcc_symbol_table *table = mcc_symbol_table_create((&parser_result)->program);
+	struct mcc_semantic_check *checks = mcc_semantic_check_run_all((&parser_result)->program, table);
+	CuAssertIntEquals(tc, checks->status, MCC_SEMANTIC_CHECK_OK);
+	struct mcc_ir_row *ir = mcc_ir_generate((&parser_result)->program, table);
+	CuAssertPtrNotNull(tc, ir);
+}
+
+// clang-format off
+
+#define TESTS \
+	TEST(test1) \
+	TEST(test2)
+
+// clang-format on
+
+#include "main_stub.inc"
+#undef TESTS
