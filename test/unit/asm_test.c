139b8ee7d9347d968840a6c92eeef07cde385aaa
O.R <oliver894@gmail.com>
2020-05-22 10:38:25 +0200

    Add unit test for array declaration, Relates #167

--- test/unit/asm_test.c
+++ test/unit/asm_test.c
@@ -28,7 +28,7 @@ void test1(CuTest *tc)
 	CuAssertPtrNotNull(tc, code);
 
 	// sections
-	//CuAssertPtrEquals(tc, NULL, code->data_section);
+	// CuAssertPtrEquals(tc, NULL, code->data_section);
 	CuAssertPtrNotNull(tc, code->text_section);
 
 	// "main"
@@ -101,12 +101,46 @@ void stack_frame_size_array(CuTest *tc)
 	CuAssertIntEquals(tc, 4, code->text_section->function->head->next->next->first->literal);
 }
 
+// TODO: Add float,bool arrays
+void array_declaration(CuTest *tc)
+{
+	// Define test input and create IR
+	const char input[] = "int main(){ int[23]d;int[42]c; c[0] = 1; return 0;}";
+	struct mcc_parser_result parser_result;
+	parser_result = mcc_parse_string(input, MCC_PARSER_ENTRY_POINT_PROGRAM);
+	CuAssertIntEquals(tc, parser_result.status, MCC_PARSER_STATUS_OK);
+	struct mcc_symbol_table *table = mcc_symbol_table_create((&parser_result)->program);
+	struct mcc_semantic_check *checks = mcc_semantic_check_run_all((&parser_result)->program, table);
+	CuAssertIntEquals(tc, checks->status, MCC_SEMANTIC_CHECK_OK);
+	struct mcc_ir_row *ir = mcc_ir_generate((&parser_result)->program, table);
+	CuAssertPtrNotNull(tc, ir);
+
+	struct mcc_asm *code = mcc_asm_generate(ir);
+	CuAssertPtrNotNull(tc, code);
+
+	CuAssertPtrNotNull(tc, code->data_section);
+	CuAssertPtrNotNull(tc, code->text_section);
+
+	// First declaration: int[23]d
+	CuAssertPtrNotNull(tc, code->data_section->head);
+	CuAssertIntEquals(tc, 23, code->data_section->head->array_size);
+	CuAssertStrEquals(tc, "d", code->data_section->head->identifier);
+	CuAssertIntEquals(tc, MCC_ASM_DECLARATION_TYPE_ARRAY, code->data_section->head->type);
+
+	// Second declaration: int[42]c
+	CuAssertPtrNotNull(tc, code->data_section->head->next);
+	CuAssertIntEquals(tc, 42, code->data_section->head->next->array_size);
+	CuAssertStrEquals(tc, "c", code->data_section->head->next->identifier);
+	CuAssertIntEquals(tc, MCC_ASM_DECLARATION_TYPE_ARRAY, code->data_section->head->next->type);
+}
+
 // clang-format off
 
 #define TESTS \
 	TEST(test1) \
 	TEST(stack_frame_size_int) \
-	TEST(stack_frame_size_array)
+	TEST(stack_frame_size_array) \
+	TEST(array_declaration)
 
 // clang-format on
 
