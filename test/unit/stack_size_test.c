f53a290e829ddf084dee5bb05f6d248cdedf3048
Oliver Roß <oliver.ross@student.uibk.ac.at>
2020-06-12 10:14:42 +0200

    Add unit tests

--- test/unit/stack_size_test.c
+++ test/unit/stack_size_test.c
@@ -107,7 +107,7 @@ void test_int_array(CuTest *tc)
 void test_int_multiple_references(CuTest *tc)
 {
 	// Define test input and create IR -> produces 2 temporaries
-	const char input[] = "int main(){int a; a = 1;int b; b = 1; a = 2; return 0;}";
+	const char input[] = "int main(){int a; a = 1;int b; b = 1; a = 2;int c; c = 0; b = 1; c = 2; return 0;}";
 	struct mcc_parser_result parser_result;
 	parser_result = mcc_parse_string(input, MCC_PARSER_ENTRY_POINT_PROGRAM);
 	CuAssertIntEquals(tc, parser_result.status, MCC_PARSER_STATUS_OK);
@@ -121,10 +121,48 @@ void test_int_multiple_references(CuTest *tc)
 
 	// an_ir
 	CuAssertPtrNotNull(tc, an_ir);
-	CuAssertIntEquals(tc, 2 * STACK_SIZE_INT, an_ir->stack_size);
+	// Func label "main"
+	CuAssertIntEquals(tc, 3 * STACK_SIZE_INT, an_ir->stack_size);
+	// "a = 1"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -STACK_SIZE_INT, an_ir->stack_position);
+	// "b = 1"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -2 * STACK_SIZE_INT, an_ir->stack_position);
+	// "a = 2"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -STACK_SIZE_INT, an_ir->stack_position);
+	// "c = 0"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -3 * STACK_SIZE_INT, an_ir->stack_position);
+	// "b = 1"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -2 * STACK_SIZE_INT, an_ir->stack_position);
+	// "c = 2"
+	an_ir = an_ir->next;
+	CuAssertIntEquals(tc, -3 * STACK_SIZE_INT, an_ir->stack_position);
+}
+
+void test_bool_int(CuTest *tc)
+{
+	// Define test input and create IR -> produces 2 temporaries
+	const char input[] = "int main(){int a; a = 1;bool b; b = true; a = 2; return 0;}";
+	struct mcc_parser_result parser_result;
+	parser_result = mcc_parse_string(input, MCC_PARSER_ENTRY_POINT_PROGRAM);
+	CuAssertIntEquals(tc, parser_result.status, MCC_PARSER_STATUS_OK);
+	struct mcc_symbol_table *table = mcc_symbol_table_create((&parser_result)->program);
+	struct mcc_semantic_check *checks = mcc_semantic_check_run_all((&parser_result)->program, table);
+	CuAssertIntEquals(tc, checks->status, MCC_SEMANTIC_CHECK_OK);
+	struct mcc_ir_row *ir = mcc_ir_generate((&parser_result)->program, table);
+	CuAssertPtrNotNull(tc, ir);
+
+	struct mcc_annotated_ir *an_ir = mcc_annotate_ir(ir);
+
+	// an_ir
+	CuAssertPtrNotNull(tc, an_ir);
+	CuAssertIntEquals(tc, STACK_SIZE_INT + STACK_SIZE_BOOL, an_ir->stack_size);
 	CuAssertIntEquals(tc, -STACK_SIZE_INT, an_ir->next->stack_position);
-	CuAssertIntEquals(tc, -2*STACK_SIZE_INT, an_ir->next->next->stack_position);
-	// CuAssertIntEquals(tc, -STACK_SIZE_INT, an_ir->next->next->next->stack_position);
+	CuAssertIntEquals(tc, -(STACK_SIZE_INT + STACK_SIZE_BOOL), an_ir->next->next->stack_position);
 }
 
 // clang-format off
